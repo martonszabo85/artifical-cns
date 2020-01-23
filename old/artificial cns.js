904e05075dde9420b77409e4a0e0a95a3f983a0b
Marton Szabo <martonszabo85@gmail.com>
2020-01-23 01:22:34 +0100

    Added old versions into old folder

---
+++ old/artificial cns.js
@@ -0,0 +1,10 @@
+const cWidth = 1855;
+const cHeight = 1010;
+
+const canvas = document.createElement(id = "canvas");
+canvas.id = "canvas"
+canvas.width = cWidth;
+canvas.height = cHeight;
+document.body.insertBefore(canvas, document.body.childNodes[0]);
+
+const ctx = canvas.getContext("2d");
